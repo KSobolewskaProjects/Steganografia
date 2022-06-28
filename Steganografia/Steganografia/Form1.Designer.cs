4dc0e0c99bede7dd752d6a03b9031909baa5e198
Karolina <108337709+KSobolewskaProjects@users.noreply.github.com>
2022-06-28 17:50:40 +0200

    Add files via upload

---
+++ Steganografia/Steganografia/Form1.Designer.cs
@@ -0,0 +1,259 @@
+﻿
+namespace Steganografia
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ofdOknoDialogowe = new System.Windows.Forms.OpenFileDialog();
+            this.btnZaladujPlik = new System.Windows.Forms.Button();
+            this.lblZaladowaneZdjecie = new System.Windows.Forms.Label();
+            this.lblWprowadzTekst = new System.Windows.Forms.Label();
+            this.tbWprowadzanyTekst = new System.Windows.Forms.RichTextBox();
+            this.btnUkryjTekst = new System.Windows.Forms.Button();
+            this.tabUkryj = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.lblGdzieZapisanoZmieniony = new System.Windows.Forms.Label();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.btnOdczytajWiadomosc = new System.Windows.Forms.Button();
+            this.tbDaneWyjsciowe = new System.Windows.Forms.RichTextBox();
+            this.lblSciezkaZmienione = new System.Windows.Forms.Label();
+            this.btnWybierzZmienione = new System.Windows.Forms.Button();
+            this.lblSciezkaOrginal = new System.Windows.Forms.Label();
+            this.btnWybierzOrginal = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.tabUkryj.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ofdOknoDialogowe
+            // 
+            this.ofdOknoDialogowe.FileName = "doukrycia";
+            this.ofdOknoDialogowe.Title = "Wybierz zdjęcie";
+            // 
+            // btnZaladujPlik
+            // 
+            this.btnZaladujPlik.Location = new System.Drawing.Point(20, 17);
+            this.btnZaladujPlik.Name = "btnZaladujPlik";
+            this.btnZaladujPlik.Size = new System.Drawing.Size(200, 30);
+            this.btnZaladujPlik.TabIndex = 0;
+            this.btnZaladujPlik.Text = "Załaduj zdjęcie";
+            this.btnZaladujPlik.UseVisualStyleBackColor = true;
+            this.btnZaladujPlik.Click += new System.EventHandler(this.przyciskZaladujPlik_Click);
+            // 
+            // lblZaladowaneZdjecie
+            // 
+            this.lblZaladowaneZdjecie.AutoSize = true;
+            this.lblZaladowaneZdjecie.Location = new System.Drawing.Point(274, 25);
+            this.lblZaladowaneZdjecie.Name = "lblZaladowaneZdjecie";
+            this.lblZaladowaneZdjecie.Size = new System.Drawing.Size(93, 15);
+            this.lblZaladowaneZdjecie.TabIndex = 2;
+            this.lblZaladowaneZdjecie.Text = "Wybrane zdjęcie";
+            this.lblZaladowaneZdjecie.Visible = false;
+            // 
+            // lblWprowadzTekst
+            // 
+            this.lblWprowadzTekst.AutoSize = true;
+            this.lblWprowadzTekst.Location = new System.Drawing.Point(41, 85);
+            this.lblWprowadzTekst.Name = "lblWprowadzTekst";
+            this.lblWprowadzTekst.Size = new System.Drawing.Size(149, 15);
+            this.lblWprowadzTekst.TabIndex = 3;
+            this.lblWprowadzTekst.Text = "Wprowadz tekst do ukrycia";
+            this.lblWprowadzTekst.Visible = false;
+            // 
+            // tbWprowadzanyTekst
+            // 
+            this.tbWprowadzanyTekst.Location = new System.Drawing.Point(274, 85);
+            this.tbWprowadzanyTekst.Name = "tbWprowadzanyTekst";
+            this.tbWprowadzanyTekst.Size = new System.Drawing.Size(236, 200);
+            this.tbWprowadzanyTekst.TabIndex = 4;
+            this.tbWprowadzanyTekst.Text = "";
+            this.tbWprowadzanyTekst.Visible = false;
+            // 
+            // btnUkryjTekst
+            // 
+            this.btnUkryjTekst.Location = new System.Drawing.Point(79, 306);
+            this.btnUkryjTekst.Name = "btnUkryjTekst";
+            this.btnUkryjTekst.Size = new System.Drawing.Size(75, 23);
+            this.btnUkryjTekst.TabIndex = 5;
+            this.btnUkryjTekst.Text = "Ukryj tekst";
+            this.btnUkryjTekst.UseVisualStyleBackColor = true;
+            this.btnUkryjTekst.Visible = false;
+            this.btnUkryjTekst.Click += new System.EventHandler(this.btnUkryjTekst_Click);
+            // 
+            // tabUkryj
+            // 
+            this.tabUkryj.Controls.Add(this.tabPage1);
+            this.tabUkryj.Controls.Add(this.tabPage2);
+            this.tabUkryj.Location = new System.Drawing.Point(2, 3);
+            this.tabUkryj.Name = "tabUkryj";
+            this.tabUkryj.SelectedIndex = 0;
+            this.tabUkryj.Size = new System.Drawing.Size(624, 559);
+            this.tabUkryj.TabIndex = 8;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.lblGdzieZapisanoZmieniony);
+            this.tabPage1.Controls.Add(this.btnZaladujPlik);
+            this.tabPage1.Controls.Add(this.lblWprowadzTekst);
+            this.tabPage1.Controls.Add(this.btnUkryjTekst);
+            this.tabPage1.Controls.Add(this.tbWprowadzanyTekst);
+            this.tabPage1.Controls.Add(this.lblZaladowaneZdjecie);
+            this.tabPage1.Location = new System.Drawing.Point(4, 24);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(616, 531);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Ukryj";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // lblGdzieZapisanoZmieniony
+            // 
+            this.lblGdzieZapisanoZmieniony.AutoSize = true;
+            this.lblGdzieZapisanoZmieniony.Location = new System.Drawing.Point(20, 360);
+            this.lblGdzieZapisanoZmieniony.Name = "lblGdzieZapisanoZmieniony";
+            this.lblGdzieZapisanoZmieniony.Size = new System.Drawing.Size(179, 15);
+            this.lblGdzieZapisanoZmieniony.TabIndex = 6;
+            this.lblGdzieZapisanoZmieniony.Text = "Ścieżka zapisanego zmienionego";
+            this.lblGdzieZapisanoZmieniony.Visible = false;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.btnOdczytajWiadomosc);
+            this.tabPage2.Controls.Add(this.tbDaneWyjsciowe);
+            this.tabPage2.Controls.Add(this.lblSciezkaZmienione);
+            this.tabPage2.Controls.Add(this.btnWybierzZmienione);
+            this.tabPage2.Controls.Add(this.lblSciezkaOrginal);
+            this.tabPage2.Controls.Add(this.btnWybierzOrginal);
+            this.tabPage2.Location = new System.Drawing.Point(4, 24);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(616, 531);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Odczytaj";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // btnOdczytajWiadomosc
+            // 
+            this.btnOdczytajWiadomosc.Location = new System.Drawing.Point(30, 140);
+            this.btnOdczytajWiadomosc.Name = "btnOdczytajWiadomosc";
+            this.btnOdczytajWiadomosc.Size = new System.Drawing.Size(173, 23);
+            this.btnOdczytajWiadomosc.TabIndex = 5;
+            this.btnOdczytajWiadomosc.Text = "Odczytaj ukrytą wiadomość";
+            this.btnOdczytajWiadomosc.UseVisualStyleBackColor = true;
+            this.btnOdczytajWiadomosc.Click += new System.EventHandler(this.btnOdczytajWiadomosc_Click);
+            // 
+            // tbDaneWyjsciowe
+            // 
+            this.tbDaneWyjsciowe.Enabled = false;
+            this.tbDaneWyjsciowe.Location = new System.Drawing.Point(27, 168);
+            this.tbDaneWyjsciowe.Name = "tbDaneWyjsciowe";
+            this.tbDaneWyjsciowe.Size = new System.Drawing.Size(553, 350);
+            this.tbDaneWyjsciowe.TabIndex = 4;
+            this.tbDaneWyjsciowe.Text = "";
+            // 
+            // lblSciezkaZmienione
+            // 
+            this.lblSciezkaZmienione.AutoSize = true;
+            this.lblSciezkaZmienione.Location = new System.Drawing.Point(250, 85);
+            this.lblSciezkaZmienione.Name = "lblSciezkaZmienione";
+            this.lblSciezkaZmienione.Size = new System.Drawing.Size(0, 15);
+            this.lblSciezkaZmienione.TabIndex = 3;
+            // 
+            // btnWybierzZmienione
+            // 
+            this.btnWybierzZmienione.Location = new System.Drawing.Point(30, 80);
+            this.btnWybierzZmienione.Name = "btnWybierzZmienione";
+            this.btnWybierzZmienione.Size = new System.Drawing.Size(173, 23);
+            this.btnWybierzZmienione.TabIndex = 1;
+            this.btnWybierzZmienione.Text = "Wybierz zmienione zdjęcie";
+            this.btnWybierzZmienione.UseVisualStyleBackColor = true;
+            this.btnWybierzZmienione.Click += new System.EventHandler(this.btnWybierzZmienione_Click);
+            // 
+            // lblSciezkaOrginal
+            // 
+            this.lblSciezkaOrginal.AutoSize = true;
+            this.lblSciezkaOrginal.Location = new System.Drawing.Point(250, 25);
+            this.lblSciezkaOrginal.Name = "lblSciezkaOrginal";
+            this.lblSciezkaOrginal.Size = new System.Drawing.Size(0, 15);
+            this.lblSciezkaOrginal.TabIndex = 2;
+            // 
+            // btnWybierzOrginal
+            // 
+            this.btnWybierzOrginal.Location = new System.Drawing.Point(30, 20);
+            this.btnWybierzOrginal.Name = "btnWybierzOrginal";
+            this.btnWybierzOrginal.Size = new System.Drawing.Size(173, 23);
+            this.btnWybierzOrginal.TabIndex = 0;
+            this.btnWybierzOrginal.Text = "Wybierz orginalne zdjecie ";
+            this.btnWybierzOrginal.UseVisualStyleBackColor = true;
+            this.btnWybierzOrginal.Click += new System.EventHandler(this.btnWybierzOrginal_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "doukrycia";
+            this.openFileDialog1.Title = "Wybierz zdjęcie";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(625, 574);
+            this.Controls.Add(this.tabUkryj);
+            this.Name = "Form1";
+            this.Text = "Steganografia by Karolina Sobolewska";
+            this.tabUkryj.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog ofdOknoDialogowe;
+        private System.Windows.Forms.Button btnZaladujPlik;
+        private System.Windows.Forms.Label lblZaladowaneZdjecie;
+        private System.Windows.Forms.Label lblWprowadzTekst;
+        private System.Windows.Forms.RichTextBox tbWprowadzanyTekst;
+        private System.Windows.Forms.Button btnUkryjTekst;
+        private System.Windows.Forms.TabControl tabUkryj;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label lblSciezkaZmienione;
+        private System.Windows.Forms.Button btnWybierzZmienione;
+        private System.Windows.Forms.Label lblSciezkaOrginal;
+        private System.Windows.Forms.Button btnWybierzOrginal;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.RichTextBox tbDaneWyjsciowe;
+        private System.Windows.Forms.Label lblGdzieZapisanoZmieniony;
+        private System.Windows.Forms.Button btnOdczytajWiadomosc;
+    }
+}
+
---
+++ Steganografia/Steganografia/Form1.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Steganografia
+{
+    public partial class Form1 : Form
+    {
+        OpenFileDialog oknoDialogowe = new OpenFileDialog();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void przyciskZaladujPlik_Click(object sender, EventArgs e)
+        {
+            //ustawienia okna dialogowego
+            oknoDialogowe.InitialDirectory = @"C:\Users\user\Desktop";
+            oknoDialogowe.Filter = "All files (*.*)|*.*";
+            oknoDialogowe.ShowDialog();
+            //instrukcje do wykonania po załadowaniu tekstu
+            if (oknoDialogowe.FileName != "")
+            {
+                //zmiana ustawień przycisków
+                lblZaladowaneZdjecie.Visible = true;
+                lblZaladowaneZdjecie.Text = oknoDialogowe.FileName;
+                lblWprowadzTekst.Visible = true;
+                tbWprowadzanyTekst.Visible = true;
+                btnUkryjTekst.Visible = true;
+
+            }
+            else
+            {
+                //informacje o braku wyboru obrazka
+                lblZaladowaneZdjecie.Text = "Nie masz wybranego zdjęcia";
+            }
+        }
+
+        private void btnUkryjTekst_Click(object sender, EventArgs e)
+        {
+
+            //bitmapa z wybranego obrazka
+            Bitmap obrazek = new Bitmap(oknoDialogowe.FileName);
+            //sprawdzenie czy wprowadzony tekst nie jest za długi
+            if (tbWprowadzanyTekst.Text.Length < (obrazek.Width * obrazek.Height / 2))
+            {
+                //zmiana widoczności przycisków
+                btnZaladujPlik.Enabled = false;
+                tbWprowadzanyTekst.Enabled = false;
+                btnUkryjTekst.Enabled = false;
+                //zmiana tekstu na bin
+                StringBuilder sb = new StringBuilder();
+                foreach (char litera in tbWprowadzanyTekst.Text.ToCharArray())
+                {
+                    sb.Append(Convert.ToString(litera, 2).PadLeft(8, '0'));
+                }
+                String tekstWZeroJedynkach = Convert.ToString(sb);
+
+                //licznik znaków potrzebny do przejścia stringa
+                int licznik = 0;
+                byte odejmijOdA = 0;
+                byte odejmijOdR = 0;
+                byte odejmijOdG = 0;
+                byte odejmijOdB = 0;
+
+                //for do przechodzenia po każdym pikselu
+                for (int i = 0; i < obrazek.Width - 1; i++)
+                {
+                    for (int j = 0; j < obrazek.Height - 1; j++)
+                    {
+                        //pobierz kolor pixela z obrazka
+                        Color pixel = obrazek.GetPixel(j, i);
+                        switch (tekstWZeroJedynkach.Length)
+                        {
+
+                            case 0:
+                                break;
+
+                            case 1:
+                                if (tekstWZeroJedynkach[licznik].Equals('1')) { odejmijOdA++; }
+                                tekstWZeroJedynkach = tekstWZeroJedynkach.Substring(1);
+                                break;
+
+                            case 2:
+                                if (tekstWZeroJedynkach[licznik].Equals('1')) { odejmijOdA++; }
+                                if (tekstWZeroJedynkach[licznik + 1].Equals('1')) { odejmijOdR++; }
+                                tekstWZeroJedynkach = tekstWZeroJedynkach.Substring(2);
+                                break;
+
+                            case 3:
+                                if (tekstWZeroJedynkach[licznik].Equals('1')) { odejmijOdA++; }
+                                if (tekstWZeroJedynkach[licznik + 1].Equals('1')) { odejmijOdR++; }
+                                if (tekstWZeroJedynkach[licznik + 2].Equals('1')) { odejmijOdG++; }
+                                tekstWZeroJedynkach = tekstWZeroJedynkach.Substring(3);
+                                break;
+
+                            default:
+                                if (tekstWZeroJedynkach[licznik].Equals('1')) { odejmijOdA++; }
+                                if (tekstWZeroJedynkach[licznik + 1].Equals('1')) { odejmijOdR++; }
+                                if (tekstWZeroJedynkach[licznik + 2].Equals('1')) { odejmijOdG++; }
+                                if (tekstWZeroJedynkach[licznik + 3].Equals('1')) { odejmijOdB++; }
+                                tekstWZeroJedynkach = tekstWZeroJedynkach.Substring(4);
+                                break;
+                        }
+
+                        //ustaw kolor zgodnie z przesunięciem z prowadzanego tekstu
+                        Color pixelNowy = Color.FromArgb(pixel.A - odejmijOdA, pixel.R - odejmijOdR, pixel.G - odejmijOdG, pixel.B - odejmijOdB);
+                        //zmień kolor piksela na wybranym obrazku
+                        obrazek.SetPixel(j, i, pixelNowy);
+                        //zerowanie zmiennych
+                        odejmijOdA = odejmijOdB = odejmijOdG = odejmijOdR = 0;
+                    }
+                }
+                //zapisywanie obrazka 
+                obrazek.Save(@"C:\Users\user\Desktop\ukryte.jpg");
+                //informacja gdzie znajdziemy zmieniony plik
+                lblGdzieZapisanoZmieniony.Visible = true;
+                //informacja gdzie znajdziemy zmieniony obrazek
+                lblGdzieZapisanoZmieniony.Text = "Obrazek, z ukrytym w nim tekstem, znajdziesz " + @"C:\Users\user\Desktop\ukryte.jpg";
+            }
+            else { MessageBox.Show("Wprowadzony tekst jest za długi"); tbWprowadzanyTekst.Text = ""; }
+        }
+
+        private void btnWybierzOrginal_Click(object sender, EventArgs e)
+        {
+            //ustawienia okna dialogowego
+            oknoDialogowe.InitialDirectory = @"C:\Users\user\Desktop";
+            oknoDialogowe.Filter = "All files (*.*)|*.*";
+            oknoDialogowe.ShowDialog();
+            //sprawdzanie czy wybrano jakieś zdjęcie
+            if (oknoDialogowe.FileName != "")
+            {
+                //wyświetlenie ścieżki wybranego zdjęcia
+                lblSciezkaOrginal.Text = oknoDialogowe.FileName;                
+            }
+            else
+            {
+                //informacje o braku wyboru obrazka
+                lblSciezkaOrginal.Text = "Nie masz wybranego zdjęcia";
+            }            
+        }
+        
+
+        private void btnWybierzZmienione_Click(object sender, EventArgs e)
+        {
+            //ustawienia okna dialogowego
+            oknoDialogowe.InitialDirectory = @"C:\Users\user\Desktop";
+            oknoDialogowe.Filter = "All files (*.*)|*.*";
+            oknoDialogowe.ShowDialog();
+            //sprawdzanie czy wybrano jakieś zdjęcie
+            if (oknoDialogowe.FileName != "")
+            {
+                //wyświetlenie ścieżki wybranego zdjęcia
+                lblSciezkaZmienione.Text = oknoDialogowe.FileName;
+            }
+            else
+            {
+                //informacje o braku wyboru obrazka
+                lblSciezkaZmienione.Text = "Nie masz wybranego zdjęcia";
+            }
+        }
+        //funkcja do zmiany z zapisu bin na tekst
+        public Byte[] GetBytesFromBinaryString(String binary)
+        {
+            var list = new List<Byte>();
+
+            for (int i = 0; i < binary.Length; i += 8)
+            {
+                if (i + 8 < binary.Length)
+                {
+                    String t = binary.Substring(i, 8);
+                    list.Add(Convert.ToByte(t, 2));
+                }
+            }
+
+            return list.ToArray();
+        }
+        private void btnOdczytajWiadomosc_Click(object sender, EventArgs e)
+        {
+            //zmieniam przyciski na niedostępne
+            btnWybierzOrginal.Enabled = false;
+            btnWybierzZmienione.Enabled = false;
+            //Tworze dwie bitmapy każdego z obrazków
+            Bitmap orginal = new Bitmap(lblSciezkaOrginal.Text);
+            Bitmap zmieniony = new Bitmap(lblSciezkaZmienione.Text);
+
+            //zmienna przechowująca różnicę w wartości bitów
+            int roznica = 0;
+            int licznik = 0;
+            //zmienna do danych wyjściowych
+            string daneWyjsciowe = "";
+            do
+            {
+                for (int i = 0; i < orginal.Height - 1; i++)
+                {
+                    for (int j = 0; j < orginal.Width - 1; j++)
+                    {
+                        //pobierz kolor pixela z obrazka Orginalnego i Zmienionego
+                        Color pixelOrginal = orginal.GetPixel(j, i);
+                        Color pixelZmieniony = zmieniony.GetPixel(j, i);
+                        //zapisywanie rożnicy ARGB do danych wyjściowych
+                        roznica = pixelOrginal.A - pixelZmieniony.A;
+                        if (roznica == 0) { licznik++; } else { licznik = 0; }
+                        daneWyjsciowe = daneWyjsciowe + Convert.ToString(roznica);
+                        roznica = pixelOrginal.R - pixelZmieniony.R;
+                        if (roznica == 0) { licznik++; } else { licznik = 0; }
+                        daneWyjsciowe = daneWyjsciowe + Convert.ToString(roznica);
+                        roznica = pixelOrginal.G - pixelZmieniony.G;
+                        if (roznica == 0) { licznik++; } else { licznik = 0; }
+                        daneWyjsciowe = daneWyjsciowe + Convert.ToString(roznica);
+                        roznica = pixelOrginal.B - pixelZmieniony.B;
+                        if (roznica == 0) { licznik++; } else { licznik = 0; }
+                        daneWyjsciowe = daneWyjsciowe + Convert.ToString(roznica);
+                    }
+                }
+            } while (licznik < 128);
+            tbDaneWyjsciowe.Text = Encoding.ASCII.GetString(GetBytesFromBinaryString(daneWyjsciowe));
+
+
+        }
+    }
+    
+}
